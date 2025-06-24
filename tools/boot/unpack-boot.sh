13862357b1f7a6b6cad8e9615688fcc22020e0c7
yifengyou <842056007@qq.com>
2025-06-24 22:44:14 +0800

    update boot.img unpack/pack scripts
    Signed-off-by: yifengyou <842056007@qq.com>

--- tools/boot/unpack-boot.sh
+++ tools/boot/unpack-boot.sh
@@ -14,18 +14,34 @@ if [ ! -f ${IMG} ] ; then
 	exit 1
 fi
 
-mkdir -p boot-unpack
-dumpimage -i ${IMG} -T flat_dt  -p 0 boot-unpack/boot_fdt
-ls -alh boot-unpack/boot_fdt
 
-dumpimage -i ${IMG} -T flat_dt  -p 1 boot-unpack/boot_kernel
-ls -alh boot-unpack/boot_kernel
+file_info=$(file -b ${IMG})
+if [[ "$file_info" =~ "Device Tree Blob version" ]]; then
+	echo "Device Tree Blob version"
+	mkdir -p boot-unpack
+	dumpimage -i ${IMG} -T flat_dt  -p 0 boot-unpack/boot_fdt
+	ls -alh boot-unpack/boot_fdt
 
-dumpimage -i ${IMG} -T flat_dt  -p 2 boot-unpack/resource
-ls -alh boot-unpack/boot_resource
+	dumpimage -i ${IMG} -T flat_dt  -p 1 boot-unpack/boot_kernel
+	ls -alh boot-unpack/boot_kernel
+
+	dumpimage -i ${IMG} -T flat_dt  -p 2 boot-unpack/boot_resource
+	ls -alh boot-unpack/boot_resource
+	cd boot-unpack
+	resource_tool  --unpack --image=boot_resource
+	cd ${WORKDIR}
+	tree boot-unpack
+elif [[ "$file_info" =~ "Android bootimg" ]]; then
+	echo "Android bootimg"
+	mkdir -p boot-unpack
+	unpack_bootimg --boot_img ${IMG} --out boot-unpack
+	cd boot-unpack
+	resource_tool --unpack --image=second
+	cd ${WORKDIR}
+	tree boot-unpack
+else
+	echo "unknown file type or not supported: $file_info"
+fi
 
 exit 0
-rm -rf out
-/data/rk3588/rk3588-latest/kernel/scripts/resource_tool  --unpack --image=boot_resource
-ls -alh out
 
